8f7e4205b7704ff005685ee97e75b4ce350dd912
SHILPEE GUPTA <78029920+shilpeegupta14@users.noreply.github.com>
2026-08-30 14:28:26 +0530

    initial victim page setup - navigation bottom bar tabs and screen setup

---
+++ frontend/caraxes_app/lib/pages/citizen/report_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ReportScreen extends StatelessWidget {
+  const ReportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            'Report Damage',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
