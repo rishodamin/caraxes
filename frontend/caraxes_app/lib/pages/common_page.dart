7461e67d484237f62ac0047c878b139534833bcd
rishodamin <daminrisho@gmail.com>
2026-08-30 10:44:20 +0530

    Firbase configured, Created an  empty page

---
+++ frontend/caraxes_app/lib/pages/common_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommonPage extends StatelessWidget {
+  const CommonPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
